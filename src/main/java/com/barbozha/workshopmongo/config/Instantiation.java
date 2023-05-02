670804ad69c9693c476a86e045700cc8ed793954
Barbozha <barbozha@gmail.com>
2023-05-02 17:56:00 -0300

    Operação de instanciação da base de dados

---
+++ src/main/java/com/barbozha/workshopmongo/config/Instantiation.java
@@ -0,0 +1,32 @@
+package com.barbozha.workshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.barbozha.workshopmongo.domain.User;
+import com.barbozha.workshopmongo.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		// Deleta os registros que estão na base de dados do MongoDB
+		userRepository.deleteAll();
+
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+	}
+
+}
