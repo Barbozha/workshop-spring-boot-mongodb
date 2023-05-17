2c047a4e4d0e632fe6cb585c42bda3dfa6493d5a
Barbozha <barbozha@gmail.com>
2023-05-17 15:21:26 -0300

    Consulta com vários critérios

--- src/main/java/com/barbozha/workshopmongo/resources/PostResource.java
+++ src/main/java/com/barbozha/workshopmongo/resources/PostResource.java
@@ -1,5 +1,6 @@
 package com.barbozha.workshopmongo.resources;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,6 +42,16 @@ public class PostResource {
 		return ResponseEntity.ok().body(list);
 	}
 	
-	
+	@RequestMapping(value = "/localizafull", method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> fullSearch(
+			@RequestParam(value = "text", defaultValue = "") String text,
+			@RequestParam(value = "minDate", defaultValue = "") String minDate,
+			@RequestParam(value = "maxDate", defaultValue = "") String maxDate) {
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+		List<Post> list = service.fullSearch(text, min, max);
+		return ResponseEntity.ok().body(list);
+	}
 	
 }
